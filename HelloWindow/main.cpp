f117d3851f0090eee0f1e56987e03809e6b4ed0b
HAFSIA GHASSEN <110825502+GhassenHafsiaINSAT@users.noreply.github.com>
2024-09-10 10:53:39 +0100

    setting the color of background to green
    some issues but fixed, with resizing screen.

--- HelloWindow/main.cpp
+++ HelloWindow/main.cpp
@@ -1,12 +1,18 @@
 #include "include/glad.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
-/*
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
-*/
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) // Check if the user pressed the escape button, if not it returns GLFW_RELEASE
+        glfwSetWindowShouldClose(window, true);
+}
+
 
 int main(){
 
@@ -36,13 +42,16 @@ if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     return -1; 
 }
 
-// glViewport(0, 0, 480, 360); 
+glViewport(0, 0, 480, 360); 
 
-//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
 
 
 while(!glfwWindowShouldClose(window)){ // checks if glfw is intructed to close
 
+    processInput(window); 
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // clear screen's color buffer 
+    glClear(GL_COLOR_BUFFER_BIT); // fill the background with color buffer's content
     glfwSwapBuffers(window); // swap  color buffer to the output screen
     glfwPollEvents();   // checks if any events are triggered (keyboard input, mouse movement) 
 
